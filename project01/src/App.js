68eae2531d8368fa7810a189a6fe539b48ec699c
ImashNipun <it21034404@my.sliit.lk>
2023-01-20 10:27:50 +0530

    Create two new components
    Create NavBar and Main components
    Import them in to App.js file

--- project01/src/App.js
+++ project01/src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import Main from './components/Main';
 
 function App() {
   return (
-    <></>
+    <>
+      <NavBar />
+      <Main />
+    </>
   );
 }
 
---
+++ project01/src/components/NavBar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function NavBar(){
+    return(
+        <h1 className="navbar">NavBar gose here!</h1>
+    );
+}
