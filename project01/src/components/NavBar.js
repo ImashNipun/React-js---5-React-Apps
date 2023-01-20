64a3f8ba4af4ed4b5d38c2cd968ab0654d28cba8
ImashNipun <it21034404@my.sliit.lk>
2023-01-20 14:02:36 +0530

    Add style to navigation bar

--- project01/src/components/NavBar.js
+++ project01/src/components/NavBar.js
@@ -4,7 +4,7 @@ import logo from '../logo.svg'
 export default function NavBar(){
     return(
         <nav className="nav-container">
-            <img src={logo} alt="logo" width="100px"/>
+            <img src={logo} alt="logo" width="60px"/>
             <h3>ReactFacts</h3>
             <h4 className="p-title">React - Project 1</h4>
         </nav>
